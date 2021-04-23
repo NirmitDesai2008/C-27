88931f198cbc29b9a6963ce556ca3fb6f4bc77cb
NirmitDesai2008 <80904497+NirmitDesai2008@users.noreply.github.com>
2021-04-23 14:23:57 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,21 @@
+class slingshot{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.5,
+            length: 10
+        }
+
+        this.slingshot = Constraint.create(options);
+        World.add(world, this.slingshot);
+    }
+
+    display(){
+        var pointA = this.slingshot.bodyA.position;
+        var pointB = this.slingshot.bodyB.position;
+
+        strokeWeight(3);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
